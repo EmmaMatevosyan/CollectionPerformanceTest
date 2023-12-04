ed4b7aacb837aef0a5687e6d03a4fd7fcd5b5aea
EmmaMatevosyan <131385066+EmmaMatevosyan@users.noreply.github.com>
2023-12-04 22:34:00 +0300

    LinkedListPerformance.java

--- src/main/java/org/example/LinkedListPerformance.java
+++ src/main/java/org/example/LinkedListPerformance.java
@@ -1,2 +1,49 @@
-package org.example;public class LinkedListPerformance {
+package org.example;
+import java.util.LinkedList;
+import java.util.List;
+public class LinkedListPerformance {
+    public static long addTime;
+    public static long deleteTime;
+    public static long getTime;
+
+    public static void testLinkedList(int repetitions) {
+        //int repetitions = entered_time; // Количество повторений вызова методов
+        List<Integer> linkedList = new LinkedList<>();
+
+        // Метод add
+        long startTime = System.nanoTime();
+        for (int i = 0; i < repetitions; i++) {
+            linkedList.add(i);
+        }
+        long endTime = System.nanoTime();
+        addTime = endTime - startTime;
+
+        // Метод delete
+        startTime = System.nanoTime();
+        for (int i = repetitions - 1; i >= 0; i--) {
+            linkedList.remove(i);
+        }
+        endTime = System.nanoTime();
+        deleteTime = endTime - startTime;
+
+        // Метод get
+        for (int i = 0; i < repetitions; i++) {
+            linkedList.add(i);
+        }
+        startTime = System.nanoTime();
+        for (int i = 0; i < repetitions; i++) {
+            linkedList.get(i);
+        }
+        endTime = System.nanoTime();
+        getTime = endTime - startTime;
+
+        // Вывод результатов
+        System.out.println("\033[1;4m" + "LinkedList Performance Results:" + "\033[0m");
+        System.out.format("%-10s %-15s %-15s%n", "Method", "Repetitions", "Time in ns");
+        System.out.format("%-10s %-15d %-15d%n", "add", repetitions, addTime);
+        System.out.format("%-10s %-15d %-15d%n", "delete", repetitions, deleteTime);
+        System.out.format("%-10s %-15d %-15d%n", "get", repetitions, getTime);
+
+    }
 }
+
