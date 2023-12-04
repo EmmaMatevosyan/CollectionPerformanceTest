45b8a3c2a7fa7c8b165700cc6f3363116754741a
EmmaMatevosyan <131385066+EmmaMatevosyan@users.noreply.github.com>
2023-12-04 22:33:30 +0300

    ArrayListPerformance.java

--- src/main/java/org/example/ArrayListPerformance.java
+++ src/main/java/org/example/ArrayListPerformance.java
@@ -1,2 +1,52 @@
-package org.example;public class ArrayListPerformance {
+package org.example;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class ArrayListPerformance {
+
+    public static long addTime;
+    public static long deleteTime;
+    public static long getTime;
+
+    public static void testArrayList(int repetitions) {
+        //int repetitions = entered_time; //1000; // Количество повторений вызова методов
+        List<Integer> arrayList = new ArrayList<>();
+
+        // Метод add
+        long startTime = System.nanoTime();
+        for (int i = 0; i < repetitions; i++) {
+            arrayList.add(i);
+        }
+        long endTime = System.nanoTime();
+        addTime = endTime - startTime;
+
+        // Метод delete
+        startTime = System.nanoTime();
+        for (int i = repetitions - 1; i >= 0; i--) {
+            arrayList.remove(i);
+        }
+        endTime = System.nanoTime();
+        deleteTime = endTime - startTime;
+
+        // Метод get
+        for (int i = 0; i < repetitions; i++) {
+            arrayList.add(i);
+        }
+        startTime = System.nanoTime();
+        for (int i = 0; i < repetitions; i++) {
+            arrayList.get(i);
+        }
+
+        endTime = System.nanoTime();
+        getTime = endTime - startTime;
+
+        // Вывод результатов
+        System.out.println("\033[1;4m" + "ArrayList Performance Results:" + "\033[0m");
+        System.out.format("%-10s %-15s %-15s%n", "Method", "Repetitions", "Time in ns");
+        System.out.format("%-10s %-15d %-15d%n", "add", repetitions, addTime);
+        System.out.format("%-10s %-15d %-15d%n", "delete", repetitions, deleteTime);
+        System.out.format("%-10s %-15d %-15d%n", "get", repetitions, getTime);
+
+    }
 }
